db97c4d89335e9f39993cf03e4d519fabbfd045b
WangQi <wangqi@WangQis-MacBook-Pro.local>
2014-12-15 17:15:27 +0800

    Change the java heap from 500m to 2g

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,2 @@
 default[:logstash][:version] = "1.4"
+default[:logstash][:javaheap] = "2g"
---
+++ recipes/configure.rb
@@ -0,0 +1,12 @@
+script "config_init" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+	sed -i 's|LS_HEAP_SIZE="500m"|LS_HEAP_SIZE="<%= node[:logstash][:javaheap] %>"|' /etc/init/logstash
+  EOH
+end
+
+service 'mongos' do
+  action [:enable, :start]
+end
